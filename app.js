1f0b094ddcd40cb8e6fdab092361ce03e19c6711
saida-reddy <108296645+saida-reddy@users.noreply.github.com>
2022-11-02 17:31:05 -0500

    Ornaments

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var ornamentsRouter = require('./routes/ornaments');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/ornaments', ornamentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
